a9d5ad7ff647d868b6512f9db3a23539039ab225
Sorin Popescu <sorin.popescu@investoogroup.com>
2018-05-10 15:26:05 +0100

    Add dependency container

--- public/index.php
+++ public/index.php
@@ -1,26 +1,31 @@
 <?php
 
+use FastRoute\RouteCollector;
+use League\Plates\Engine;
+use Middlewares\Action\HomeAction;
 use Middlewares\DiactorosResponderMiddleware;
 use Middlewares\Utils\Dispatcher;
+use Pimple\Container;
+use Pimple\Psr11\Container as Psr11Container;
 use Zend\Diactoros\Response\SapiEmitter;
 use Zend\Diactoros\ServerRequestFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 
-//$container = new \Pimple\Container();
-//
-//$container[\League\Plates\Engine::class] = function ($c) {
-//        new \League\Plates\Engine();
-//    };
-//$container[\Middlewares\Action\HomeAction::class] = function ($c) {
-//        return new \Middlewares\Action\HomeAction($c[\League\Plates\Engine::class]);
-//    };
-//
-//$psr11 = new \Pimple\Psr11\Container($container);
+$container = new Container();
 
-$routeDispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) {
-    $r->addRoute('GET', '/hello/{name}', \Middlewares\Action\HomeAction::class);
+$container[Engine::class] = function ($c) {
+    return new Engine(__DIR__ . '/../src/templates/', 'phtml');
+};
+$container[HomeAction::class] = function ($c) {
+    return new HomeAction($c[Engine::class]);
+};
+
+$psr11 = new Psr11Container($container);
+
+$routeDispatcher = \FastRoute\simpleDispatcher(function (RouteCollector $r) use ($psr11) {
+    $r->addRoute('GET', '/hello/{name}', $psr11->get(HomeAction::class));
 });
 
 
--- src/Action/HomeAction.php
+++ src/Action/HomeAction.php
@@ -12,9 +12,10 @@ class HomeAction
      */
     private $template;
 
-    public function __construct()
+    public function __construct($template)
     {
-        $this->template = new Engine(__DIR__ . '/../templates/', 'phtml');
+        //$this->template = new Engine(__DIR__ . '/../templates/', 'phtml');
+        $this->template = $template;
     }
 
     public function __invoke()
@@ -22,6 +23,7 @@ class HomeAction
         $data = [
             'data' => 'Homepage title'
         ];
+
         return new HtmlResponse(
             $this->template->render('home-page', $data),
             200
